e44acd9711ee4d65cca63f58388a175553186214
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2023-11-15 21:13:51 +0600

    Реализация сохранения данных в БД

--- src/main/java/com/example/dataanalysermicroservice/model/Data.java
+++ src/main/java/com/example/dataanalysermicroservice/model/Data.java
@@ -1,5 +1,6 @@
 package com.example.dataanalysermicroservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,15 +8,24 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "data")
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Data {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Long sensorId;
     private LocalDateTime timestamp;
     private double measurement;
+
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
     public enum MeasurementType {
